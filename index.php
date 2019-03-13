4c1e75b25a5814162f0120e0d7a8aaa9a1758db9
Lukas <48499827+AfterWorldBrowsgergame@users.noreply.github.com>
2019-03-13 03:16:32 +0100

    First Step

---
+++ index.php
@@ -0,0 +1,13 @@
+<!-- Zwischen <html> und </html> spielt sich die ganze Webseite ab. Außerhalb dieser "tags" wird alles bezüglich html ignoriert -->
+<!-- Wie der tag <header> schon übersetzt heißt, ist es der "Kopf" unserer Webseite - hier kommt nicht rein, was Inhalt der Webseite ist -->
+<!-- Der <body> ist unser Webseiten Körper, hier steht der ganze Inhalt drinnen -->
+
+<html>
+	<header>
+		<title>After World</title> <!-- Das ist der Name der Webseite, welcher oben in der Taskleiste angezeigt wird - Hier wird zukünftig noch mehr drinnen stehen aber vorerst ist das für uns nicht wichtig -->
+		<link rel="stylesheet" href="css/style.css" />
+	</header>
+	<body>
+		<div align="center"><img src="img/title.png" alt="After World"></div>
+	</body>
+</html>
